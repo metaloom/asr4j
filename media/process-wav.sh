dfc50c5d694bd0dc70c02a0bf602af41162ea863
Johannes Schüth <git@jotschi.de>
2026-03-29 22:28:42 +0200

    Update testscript

--- media/process-wav.sh
+++ media/process-wav.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 MODEL=openai/whisper-large-v3
-MOVIE=jfk.webm
+MOVIE=$1
+LANG=en
 rm out.wav
-#-ss 60 -t 60 
 ffmpeg -i $MOVIE -f wav -bitexact -acodec pcm_s16le -ar 22050 -ac 1 "out.wav"
 
-curl -vvv http://localhost:8000/v1/audio/transcriptions \
+curl http://localhost:8000/v1/audio/transcriptions \
   -H "Authorization: Bearer EMPTY" \
   -F "model=$MODEL" \
-  -F "language=de" \
+  -F "language=$LANG" \
   -F "file=@out.wav"
